a6cd22de1a03d445179c21d3bf0e0c4d343e3d22
Gauthier <gauthier.pladet@gmail.com>
2022-02-01 21:37:25 +0100

    Fix animation slide css and js

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,3 +1,5 @@
+let scroll_amount = 0;
+
 async function getMovieByID(id) {
     return (await fetch("http://127.0.0.1:8000/api/v1/titles/"+id)).json()
 }
@@ -12,25 +14,33 @@ function addMovieToDivImg(data) {
         img.src = data[i].image_url
         img.classList.add("best-movie-img")
         img.setAttribute("id", "slideAnim")
-        document.getElementById("categorie_1").appendChild(img)
+        document.getElementById("categorie-1").appendChild(img)
     }
 }
 
-function clickRight() {
-    let img = document.getElementsByClassName('best-movie-img');
-    for (let i = 0; i < img.length; i++) {
-        img[i].style.animation = "slide 2s"
-    }
+document.getElementById("switch-right").onclick = function () {
+    document.getElementById("categorie-1").scrollTo({
+        top: 0,
+        left: Math.max(scroll_amount += 600, document.getElementById("categorie-1").getBoundingClientRect().width),
+        behavior: "smooth",
+    })
+}
+
+document.getElementById("switch-left").onclick = function () {
+    document.getElementById("categorie-1").scrollTo({
+        top: 0,
+        left: Math.max(scroll_amount -= 600, 0),
+        behavior: "smooth",
+    })
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         let data = []
         let i = 2
-        for (let i = 2; i < 4; i++) {
+        for (let i = 2; i < 8; i++) {
             movies = await getMoviesByGenre("Horror", i)
             movies.results.forEach(movies => data.push(movies))
-            console.log(data)
         }
         addMovieToDivImg(data)
     } catch (e) {
